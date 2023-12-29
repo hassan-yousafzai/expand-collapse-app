35da930886d7c4291e5ab3eefd3a3a52ceffa68e
Hassan <hassanakbar2012@hotmail.com>
2023-12-29 23:28:54 +0000

    refactor the code to use the toggle function rather than if statments

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-const template = document.querySelector("#list-item-template")
 document.addEventListener("click", e => {
   if (!e.target.matches(".expand-button")) return
 
   const card = e.target.closest(".card")
   const cardBody = card.querySelector(".card-body")
-  if (e.target.innerText === "Expand") {
-    cardBody.classList.add("show")
-    e.target.innerText = "Collapse"
-  } else {
-    cardBody.classList.remove("show")
-    e.target.innerText = "Expand"
-  }
-})
 
-const templateClone = template.content.cloneNode(true)
-document.body.appendChild(templateClone)
+  cardBody.classList.toggle("show")
+  e.target.innerText = e.target.innerText === "Expand" ? "Collapse" : "Expand"
+})
