278caf195d188907dea107fb5a8a0631896170fc
Hassan <hassanakbar2012@hotmail.com>
2023-12-29 23:26:57 +0000

    toggle the expand and collapse buttons to show and hide the card body

---
+++ script.js
@@ -0,0 +1,17 @@
+const template = document.querySelector("#list-item-template")
+document.addEventListener("click", e => {
+  if (!e.target.matches(".expand-button")) return
+
+  const card = e.target.closest(".card")
+  const cardBody = card.querySelector(".card-body")
+  if (e.target.innerText === "Expand") {
+    cardBody.classList.add("show")
+    e.target.innerText = "Collapse"
+  } else {
+    cardBody.classList.remove("show")
+    e.target.innerText = "Expand"
+  }
+})
+
+const templateClone = template.content.cloneNode(true)
+document.body.appendChild(templateClone)
